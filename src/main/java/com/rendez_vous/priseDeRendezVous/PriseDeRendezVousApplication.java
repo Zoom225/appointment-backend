20be05e4a71aff21626a336852000ddaac2deeaa
mise-a-jour-backend <bobypro225@gmail.com>
2026-07-26 11:01:54 +0200

    mise en place backend prise derendez vous config security

---
+++ src/main/java/com/rendez_vous/priseDeRendezVous/PriseDeRendezVousApplication.java
@@ -0,0 +1,13 @@
+package com.rendez_vous.priseDeRendezVous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriseDeRendezVousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriseDeRendezVousApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rendez_vous/priseDeRendezVous/PriseDeRendezVousApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rendez_vous.priseDeRendezVous;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriseDeRendezVousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
