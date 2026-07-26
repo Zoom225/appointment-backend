c641c45d25cd3955e68153bf626127f2ae4c0b10
mise-a-jour-backend <bobypro225@gmail.com>
2026-07-26 15:33:55 +0200

    bien add

---
+++ src/main/java/com/kangoute/appointment/entity/Role.java
@@ -0,0 +1,24 @@
+package com.kangoute.appointment.entity;
+
+import com.kangoute.appointment.enums.RoleName;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true)
+    private RoleName name;
+
+}
