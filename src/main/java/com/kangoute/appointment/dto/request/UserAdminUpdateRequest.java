4c6b6c249bd1cdaf3de26e3bf9a45b9f6cf39421
mise-a-jour-backend <bobypro225@gmail.com>
2026-07-26 22:22:54 +0200

    tout es bien en place

---
+++ src/main/java/com/kangoute/appointment/dto/request/UserAdminUpdateRequest.java
@@ -0,0 +1,69 @@
+package com.kangoute.appointment.dto.request;
+
+import com.kangoute.appointment.enums.RoleName;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+import java.util.Set;
+
+public class UserAdminUpdateRequest {
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 8)
+    private String password;
+
+    @NotEmpty
+    private Set<RoleName> roles;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<RoleName> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<RoleName> roles) {
+        this.roles = roles;
+    }
+}
