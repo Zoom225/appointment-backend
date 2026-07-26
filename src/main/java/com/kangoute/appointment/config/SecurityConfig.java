58de96fdfa711c91901bcb415a2677a7646ab151
mise-a-jour-backend <bobypro225@gmail.com>
2026-07-26 12:19:48 +0200

    mise en place

--- src/main/java/com/kangoute/appointment/config/SecurityConfig.java
+++ src/main/java/com/kangoute/appointment/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.rendez_vous.priseDeRendezVous.config;
+package com.kangoute.appointment.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
